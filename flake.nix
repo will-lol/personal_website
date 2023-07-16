bb4d4fa2eb59d36eb434630591568cd43384d8e6
Will <will.bradshaw50@gmail.com>
2023-07-16 19:29:57 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Go example flake for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, gitignore, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        overlays = [];
+        pkgs = import nixpkgs { inherit system overlays; };
+        goBuild = pkgs.buildGoModule {
+          name = "personal-website";
+          src = gitignore.lib.gitignoreSource ./.;
+	  vendorSha256 = null; 
+        };
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "personal_website";
+	  config = { Cmd = [ "${goBuild}/bin/personal-website" ]; };
+        };
+      in
+        {
+          packages = {
+            docker = dockerImage;
+            goPackage = goBuild;
+          };
+          defaultPackage = dockerImage;
+          devShell = pkgs.mkShell {
+            packages = with pkgs; [ go docker ];
+          };
+        }
+    );
+}
