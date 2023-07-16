7d0f50b16148dade33575292759e1eea66f684bb
Will <will.bradshaw50@gmail.com>
2023-07-16 21:19:30 +1000

    fix container tag

--- flake.nix
+++ flake.nix
@@ -13,19 +13,26 @@
   outputs = { self, nixpkgs, gitignore, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: 
       let
+        name = "personal-website";
+        image  = {
+          inherit name;
+	  registry = "ghcr.io";
+	  owner = "will-lol";
+	};
+
         overlays = [];
         pkgs = import nixpkgs { inherit system overlays; };
         goBuild = pkgs.buildGoModule {
-          name = "personal-website";
+	  inherit name;
           src = gitignore.lib.gitignoreSource ./.;
 	  vendorSha256 = null; 
         };
         dockerImage = pkgs.dockerTools.buildImage {
-          name = "ghcr.io/personal_website";
+          name = "${image.registry}/${image.owner}/${image.name}";
 	  config = { 
-	    Cmd = [ "${goBuild}/bin/personal-website" ]; 
+	    Cmd = [ "${goBuild}/bin/${name}" ]; 
 	    Labels = {
-	      "org.opencontainers.image.source" = "https://github.com/will-lol/personal_website";
+	      "org.opencontainers.image.source" = "https://github.com/${image.owner}/${image.name}";
 	    };
 	  };
         };
